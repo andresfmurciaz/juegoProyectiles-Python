7aab75565598f43e3552ffa36d025f090fd765b8
andresf.murciaz <andresf.murciaz@gmail.com>
2023-03-08 16:45:03 -0500

    correccion de codigo general

--- juego.py
+++ juego.py
@@ -1,26 +1,5 @@
 #importamos pygame
-import pygame, sys
-#para especificar que es lo que necesitamos en especifico y no se traiga toda la libreria
-from pygame.locals import *
-#metodo de inicializacion
-pygame.init()
-
-#constante pantalla lleva la funcion para crear nuestra pantalla de juego
-
-PANTALLA = pygame.display.set_mode((1000,600))
-
-
-#carga la img de fondo en nuestro juego y la muestra
-fondo = pygame.image.load("img/fondo2.png")
-PANTALLA.blit(fondo,(0,0))
-
-
-#carga la icono en nuestro juego y la muestra y title
-#le ponemos el tamaño a la venta y un title
-pygame.display.set_caption('juegoProyectiles-Parcial 1.')
-icono= pygame.image.load("img/icono.png")
-pygame.display.set_icon(icono)
-
+import pygame
 
 #CODIGO DE COLOR
 BLANCO =(255,255,255)
@@ -30,18 +9,67 @@ AZUL=(0,0,255)
 
 #poner fondo de pantalla
 #PANTALLA.fill(BLANCO)
-
 #CREA UN RECTANGULO CON POSI Y TAMAÑO- parametros van por posi y despues tamaño
 #rectangulo1 = pygame.draw.rect(PANTALLA,ROJO,(100,50,100,50))
 
+#CLASE DE LA NAVE
+class Nave(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((200,200))
+        self.image.fill(BLANCO)
+        self.rect = self.image.get_rect()
+        self.rect.center= (W // 2, H // 2)
+
+#actualiza cada vez que el bucle de una vuelta
+    def update(self):
+        self.rect.y += 10
+        if self.rect.top > H:
+            self.rect.bottom=0
+
+#-------------------------------------------------------------
+#metodo de inicializacion
+pygame.init()
+
+#constante pantalla lleva la funcion para crear nuestra pantalla de juego
+W,H=1000,600
+pantalla = pygame.display.set_mode((W,H))
+#ACELERAR O DESACELERAR EL JUEGO
+FPS =500
+RELOJ= pygame.time.Clock()
+#title
+pygame.display.set_caption('juegoProyectiles-Parcial 1.')
+
+#grupo de sprites , instancion de objetos
+sprites = pygame.sprite.Group()
+nave= Nave()
+sprites.add(nave)
 
 
+#bucle del juego
 
-#toca crear un bucle para que no se cierre la ventana
-while True:
+ejecutando= True
+
+while ejecutando:
+
+    # LLAMAMOS EL METODO PARA LA ACELERACION DEL FONDO
+    RELOJ.tick(FPS)
+
+    #EVENTOS
     for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    #actualiza evento para que aparezca la pantalla blanca
-    pygame.display.update()
+        if event.type == pygame.QUIT:
+            #SE TERMINA EL BUCLE
+            ejecutando=False
+
+
+    #ACTUALIZACION DE SPRITES- actuali
+    sprites.update()
+
+#LE DA EL FONDO DE PANTALLA
+    pantalla.fill(NEGRO)
+    sprites.draw(pantalla)
+    pygame.display.flip()
+
+#CIERRA LA CLASE
+pygame.quit()
+
